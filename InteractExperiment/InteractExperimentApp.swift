a1dfe1c7c82e0588081e097f153a7a8aee3be7d0
szuyun <sl798@sussex.ac.uk>
2023-05-25 15:21:25 +0100

    initialise project

---
+++ InteractExperiment/InteractExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractExperimentApp.swift
+//  InteractExperiment
+//
+//  Created by cabbage on 2023/5/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
