498584133438c4a6d060b54b323271b202dd707a
szuyun <sl798@sussex.ac.uk>
2023-05-28 19:11:00 +0100

    feat: build up app main UI structure

---
+++ InteractExperiment/ConfigurationsView.swift
@@ -0,0 +1,20 @@
+//
+//  ConfigurationsView.swift
+//  InteractExperiment
+//
+//  Created by cabbage on 2023/5/28.
+//
+
+import SwiftUI
+
+struct ConfigurationsView: View {
+    var body: some View {
+        Text("Configurations view")
+    }
+}
+
+struct ConfigurationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfigurationsView()
+    }
+}
