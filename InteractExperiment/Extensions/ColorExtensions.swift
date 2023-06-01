3bc3c3f2a60cb19073ccbf44283a3618b7d3ec64
szuyun <sl798@sussex.ac.uk>
2023-06-01 18:36:15 +0100

    chore: add colour extensions
     interactive rebase in progress; onto 1a4caf1
     Last command done (1 command done):
        pick c8efe48 chore: add colour extensions
     Next command to do (1 remaining command):
        pick 6d5c836 feat(#1): Create the process of selecting images for familiarisation and stimulus
     You are currently rebasing branch '1-ui-configuration-setup-page' on '1a4caf1'.
    
     Changes to be committed:
    	modified:   InteractExperiment.xcodeproj/project.pbxproj
    	new file:   InteractExperiment/Assets.xcassets/GrayButton.colorset/Contents.json
    	new file:   InteractExperiment/Assets.xcassets/LightGrayBackground.colorset/Contents.json
    	new file:   InteractExperiment/Assets.xcassets/RedButton.colorset/Contents.json
    	new file:   InteractExperiment/Extensions/ColorExtensions.swift

---
+++ InteractExperiment/Extensions/ColorExtensions.swift
@@ -0,0 +1,18 @@
+//
+//  ColorExtensions.swift
+//  InteractExperiment
+//
+//  Created by cabbage on 2023/5/30.
+//
+
+import SwiftUI
+
+extension Color {
+    static let button = Color.Button()
+    
+    struct Button {
+        let lightgray = Color("LightGrayBackground")
+        let gray = Color("GrayButton")
+        let red = Color("RedButton")
+    }
+}
